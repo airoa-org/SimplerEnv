c41d0305689a65e94f40865b182a3216921e93bb
haru425 <hkondoh79@gmail.com>
2025-08-05 18:17:38 +0900

    Update job.sh

--- scripts/g3_lerobotpi0/job.sh
+++ scripts/g3_lerobotpi0/job.sh
@@ -5,5 +5,7 @@ source ~/.bashrc
 source $(conda info --base)/etc/profile.d/conda.sh
 conda activate simpler_env_lerobotpi0
 
+CKPT=$1
+
 python scripts/g3_lerobotpi0/evaluate_fractal.py \
-    --ckpt-path ./scripts/g3_lerobotpi0/lerobot-pi0-fractal
+    --ckpt-path $CKPT
