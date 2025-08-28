6e832d2e75c78867008576df27166d1234e4799f
Penrose0v0 <huyang0904@outlook.com>
2025-08-28 23:31:23 +0000

    Init bridge challenge

---
+++ scripts/openpi/task_bridge_1.py
@@ -0,0 +1,24 @@
+import argparse
+
+from simpler_env.evaluation.bridge_tasks import widowx_task1_pick_object
+from simpler_env.policies.openpi.pi0_or_fast import OpenPiFastInference
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Run Comprehensive ManiSkill2 Evaluation")
+    parser.add_argument("--ckpt-path", type=str, required=True, help="Path to the checkpoint to evaluate.")
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    ckpt_path = args.ckpt_path
+
+    policy = OpenPiFastInference(saved_model_path=ckpt_path, policy_setup="widowx_bridge", action_scale=1.0)
+
+    print("Policy initialized. Starting evaluation...")
+
+    final_scores = widowx_task1_pick_object(env_policy=policy, ckpt_path=args.ckpt_path)
+
+    print("\nEvaluation finished.")
+    print(f"Final calculated scores: {final_scores}")
---
+++ simpler_env/evaluation/bridge_tasks.py
@@ -0,0 +1,107 @@
+from typing import Any, Dict, List
+
+from ..policies.base import AiroaBasePolicy
+from .config import ManiSkill2Config
+from .evaluate import _run_single_evaluation
+
+from . import random_envs
+
+
+def widowx_task1_pick_object(env_policy: AiroaBasePolicy, ckpt_path: str) -> List[List[bool]]:
+    results: List[List[bool]] = []
+
+    cfg = ManiSkill2Config(
+        policy_setup="widowx_bridge",
+        robot="widowx",
+        control_freq=5,
+        sim_freq=500,
+        max_episode_steps=60,
+        env_name="GraspRandomObjectInScene-v0",
+        scene_name="bridge_table_1_v1",
+        rgb_overlay_path="ManiSkill2_real2sim/data/real_inpainting/bridge_real_eval_1.png",
+        robot_init_x_range=[0.147, 0.147, 1],
+        robot_init_y_range=[0.028, 0.028, 1],
+        obj_variation_mode="episode",
+        obj_episode_range=[0, 24],
+        robot_init_rot_quat_center=[0, 0, 0, 1],
+        robot_init_rot_rpy_range=[0, 0, 1, 0, 0, 1, 0, 0, 1],
+        ckpt_path=ckpt_path,
+    )
+    results.append(_run_single_evaluation(env_policy, cfg, ckpt_path))
+
+    return results
+
+
+def widowx_task2_stack_cube(env_policy: AiroaBasePolicy, ckpt_path: str) -> List[List[bool]]:
+    results: List[List[bool]] = []
+
+    cfg = ManiSkill2Config(
+        policy_setup="widowx_bridge",
+        robot="widowx",
+        control_freq=5,
+        sim_freq=500,
+        max_episode_steps=60,
+        env_name="StackRandomGreenYellowCubeInScene-v0",
+        scene_name="bridge_table_1_v1",
+        rgb_overlay_path="ManiSkill2_real2sim/data/real_inpainting/bridge_real_eval_1.png",
+        robot_init_x_range=[0.147, 0.147, 1],
+        robot_init_y_range=[0.028, 0.028, 1],
+        obj_variation_mode="episode",
+        obj_episode_range=[0, 24],
+        robot_init_rot_quat_center=[0, 0, 0, 1],
+        robot_init_rot_rpy_range=[0, 0, 1, 0, 0, 1, 0, 0, 1],
+        ckpt_path=ckpt_path,
+    )
+    results.append(_run_single_evaluation(env_policy, cfg, ckpt_path))
+
+    return results
+
+
+def widowx_task3_put_object_on_top(env_policy: AiroaBasePolicy, ckpt_path: str) -> List[List[bool]]:
+    results: List[List[bool]] = []
+
+    cfg = ManiSkill2Config(
+        policy_setup="widowx_bridge",
+        robot="widowx",
+        control_freq=5,
+        sim_freq=500,
+        max_episode_steps=60,
+        env_name="PutRandomObjectOnRandomTopInScene-v0",
+        scene_name="bridge_table_1_v1",
+        rgb_overlay_path="ManiSkill2_real2sim/data/real_inpainting/bridge_real_eval_1.png",
+        robot_init_x_range=[0.147, 0.147, 1],
+        robot_init_y_range=[0.028, 0.028, 1],
+        obj_variation_mode="episode",
+        obj_episode_range=[0, 24],
+        robot_init_rot_quat_center=[0, 0, 0, 1],
+        robot_init_rot_rpy_range=[0, 0, 1, 0, 0, 1, 0, 0, 1],
+        ckpt_path=ckpt_path,
+    )
+    results.append(_run_single_evaluation(env_policy, cfg, ckpt_path))
+
+    return results
+
+
+def widowx_task4_put_object_in_basket(env_policy: AiroaBasePolicy, ckpt_path: str) -> List[List[bool]]:
+    results: List[List[bool]] = []
+
+    cfg = ManiSkill2Config(
+        policy_setup="widowx_bridge",
+        robot="widowx_sink_camera_setup",
+        control_freq=5,
+        sim_freq=500,
+        max_episode_steps=120,
+        env_name="PutRandomObjectInBasketScene-v0",
+        scene_name="bridge_table_1_v2",
+        rgb_overlay_path="ManiSkill2_real2sim/data/real_inpainting/bridge_sink.png",
+        robot_init_x_range=[0.127, 0.127, 1],
+        robot_init_y_range=[0.06, 0.06, 1],
+        obj_variation_mode="episode",
+        obj_episode_range=[0, 24],
+        robot_init_rot_quat_center=[0, 0, 0, 1],
+        robot_init_rot_rpy_range=[0, 0, 1, 0, 0, 1, 0, 0, 1],
+        ckpt_path=ckpt_path,
+    )
+    results.append(_run_single_evaluation(env_policy, cfg, ckpt_path))
+
+    return results
