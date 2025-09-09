ff4b9583983385213f244d954b1e652c2f626d77
user_00004_25b505 <user_00004_25b505@aic-login-310001.gmo-gpu.io>
2025-09-10 04:40:53 +0900

    update scripts

--- scripts/group2/evaluate_bridge.py
+++ scripts/group2/evaluate_bridge.py
@@ -3,7 +3,12 @@ import argparse
 import logging
 
 from omegaconf import DictConfig, OmegaConf
-from simpler_env.evaluation.evaluate_bridge import run_comprehensive_evaluation_bridge
+from simpler_env.evaluation.bridge_tasks import (
+    widowx_task1_pick_object,
+    widowx_task2_stack_cube,
+    widowx_task3_put_object_on_top,
+    widowx_task4_put_object_in_basket
+)
 from airoa_g2.model.policy.bridge_policy import BridgePolicy
 
 
@@ -15,6 +20,7 @@ def initialize_policy(cfg: DictConfig) -> BridgePolicy:
 def parse_args():
     parser = argparse.ArgumentParser(description="Run Comprehensive ManiSkill2 Evaluation")
     parser.add_argument("--ckpt-path", type=str, required=True, help="Path to the checkpoint to evaluate.")
+    parser.add_argument("--control-freq", type=int, default=5, help="Set control frequency (default->5)")
     return parser.parse_args()
 
 
@@ -38,8 +44,19 @@ if __name__ == "__main__":
     logger.info("Policy loaded successfully!")
 
     print("Policy initialized. Starting evaluation...")
+    tasks = [
+        widowx_task1_pick_object, 
+        widowx_task2_stack_cube, 
+        widowx_task3_put_object_on_top, 
+        widowx_task4_put_object_in_basket
+    ]
 
-    final_scores = run_comprehensive_evaluation_bridge(env_policy=policy, ckpt_path=args.ckpt_path)
+    final_scores = []
+    for task in tasks:
+        cur_scores = task(
+            env_policy=policy, ckpt_path=args.ckpt_path, control_freq=args.control_freq
+        )
+        final_scores += cur_scores
 
     print("\nEvaluation finished.")
     print(f"Final calculated scores: {final_scores}")
